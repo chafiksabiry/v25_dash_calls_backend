b7b045770509c56b1275730cdedf430650b59a53
karouma-oum <omaimaayoube149@gmail.com>
2025-05-05 10:31:15 +0100

    change model of vertex to an available one

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 //app.use(cors());
 // Enable CORS with specific configuration
 app.use(cors({
-  origin: ['https://v25-preprod.harx.ai', 'https://preprod-api-dash-calls.harx.ai','https://v25.harx.ai'],
+  origin: ['http://localhost:5180','https://v25-preprod.harx.ai', 'https://preprod-api-dash-calls.harx.ai','https://v25.harx.ai'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
