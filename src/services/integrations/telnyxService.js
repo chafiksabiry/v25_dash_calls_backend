1aabbde21453236e6624f40ab99a9c3e9cdae1d2
BILON Ikram <bilonikram20@gmail.com>
2025-10-14 11:24:59 +0100

    work with pcmu codec

--- src/services/integrations/telnyxService.js
+++ src/services/integrations/telnyxService.js
@@ -39,10 +39,13 @@ class TelnyxService {
         to: to,
         from: from,
         // Utiliser l'URL ngrok pour le streaming
-        stream_url: 'wss://api-calls.harx.ai/audio-stream',
-        stream_track: 'both_tracks',           // Écouter les deux côtés de la conversation
-        stream_bidirectional_mode: 'rtp',      // Activer le mode bidirectionnel
-        stream_bidirectional_codec: 'PCMU',    // µ-law codec standard
+        stream_url: 'wss://'+process.env.TELNYX_STREAM_URL+'/audio-stream',
+        stream_track: 'inbound_track',           // Écouter les deux côtés de la conversation
+        //stream_bidirectional_mode: 'rtp', 
+        stream_codec: 'PCMU',  // Changé de PCMU à OPUS
+        stream_sample_rate: '8000', 
+        //stream_bidirectional_codec: 'PCMU',    // Utiliser PCMU pour une meilleure compatibilité
+        //stream_bidirectional_sample_rate: 8000,  // Fréquence standard pour PCMU
         client_state: clientState,
         command_id: this.generateCommandId()
       };
